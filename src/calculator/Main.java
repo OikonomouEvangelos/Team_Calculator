7cd0d7b3cc9184bcc55510094d12526e7ba73144
Nikolas <pegiosnikolas@gmail.com>
2025-11-09 18:29:33 +0200

    Changed some typo & added mul operation

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -7,26 +7,33 @@ public class Main {
         System.out.println("Team Calculator started!");
 
         Scanner scanner=new Scanner(System.in);
-        double num1;
-        double num2;
+        double num1,num2,result=0;
         char operator;
-        double result=0;
+
 
         System.out.print("Enter the first number: ");
         num1=scanner.nextDouble();
 
-        System.out.print("Enter an operator (+, -, *, /, ^, log)");
-        operator=scanner.next().charAt(0);
 
-        System.out.print("Enter the first number: ");
+        System.out.print("Enter the second number: ");
         num2=scanner.nextDouble();
 
-        switch(operator){
-            case  '+' ->result=num1+num2;
-        }
-        System.out.println(result);
+        System.out.print("Enter an operator (+, -, *, /, ^, log)");
+        operator=scanner.next().charAt(0);
         scanner.close();
 
+       switch (operator) {
+
+           case '+':
+               result = num1 + num2;
+               break;
+           case '*':
+               result = num1 * num2;
+               break;
+
+
+       }
+        System.out.println(result);
 
     }
 }
